70ea6fe9afb899fe6b507708b47ece1aec763390
FZZkill <changzhijie11@outlook.com>
2022-12-04 11:14:18 +0800

    Completion cli code

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -1,11 +1,16 @@
 #include "cli.h"
 
+#define Version "0.0.0.1 Dev"
+#define getVersion Version" (R) Huh Devlopment"
+
 #if __GNUC__
 #define COM "[GNU GCC]"
 #elif __clang__
 #define COM "[LLVM CLANG]"
 #elif _MSC_BUILD
 #define COM "[MSFT MSVC]"
+#else
+#define COM "[UNKNOW COMPILER]"
 #endif
 #define COMP COM" [LICENSE GPL] [TYPE help() GET HELP INFO]"
 
--- src/cli/cli.h
+++ src/cli/cli.h
@@ -1,4 +1,7 @@
-#include "../main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #define CliEND 0
 
